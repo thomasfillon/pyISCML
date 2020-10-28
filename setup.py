1ba4f8605523124915eea4244b83bf4de0513f7b
Thomas Fillon <tfillon@rtsys.fr>
2020-10-28 17:34:17 +0100

    Remove numpy dependency at installation time.

--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
 from distutils.core import setup, Extension
-import numpy
+from distutils.command.build_ext import build_ext
 import os
 
+
+class CustomBuildExtCommand(build_ext):
+    """build_ext command for use when numpy headers are needed."""
+    def run(self):
+
+        # Import numpy here, only when headers are needed
+        import numpy
+
+        # Add numpy headers to include_dirs
+        self.include_dirs.append(numpy.get_include())
+
+        # Call original build_ext command
+        build_ext.run(self)
+
+        
 if os.name == 'nt':
     extra_compile_args = []
 else:
     extra_compile_args = ['--std=c++11']
+
+    
 codingExt = Extension("_pyISCML",
                       sources=["cpp/pyISCML.i", "cpp/LdpcEncode.cpp", "cpp/MpDecode.cpp", "cpp/ConvEncode.cpp", "cpp/SisoDecode.cpp"],
-                      include_dirs=[numpy.get_include()],
                       define_macros=[('SWIG_PYTHON_INTERPRETER_NO_DEBUG', 1)],
                       language="c++",
                       swig_opts=['-c++', '-py3', '-modern', '-I../include' , '-DSMALL_LONG'],
@@ -17,6 +33,8 @@ codingExt = Extension("_pyISCML",
 exts = [codingExt]
 
 setup(name="pyISCML",
+      cmdclass = {'build_ext': CustomBuildExtCommand},
+      install_requires=['numpy'],
       ext_modules=exts,
       version = '0.1',
       description = """Python Bindings for Coded Modulation Library""")
